b634ccf9c7f05a74f260f36acf377cf255742b44
haomeihong <18221050749@139.com>
2016-11-25 10:16:35 +0800

    Revit插件Version2
    数据文件、源码、测试项目
    Dto包含新的数据结构定义
    Intface包含所有新的工具类接口定义
    Impl包含所有这些工具类的接口的实现
    测试项目：目前只测试过数据读取这部分
    数据文档：包含所有的数据文档，位移的原始标志位（Q）和圈梁的标志位（Q）中的重复，这个等待后期再确认。

---
+++ TestPro/ConsoleApplication1/Program.cs
@@ -0,0 +1,63 @@
+﻿using Revit.Addin.RevitTooltip.Dto;
+using Revit.Addin.RevitTooltip.Impl;
+using Revit.Addin.RevitTooltip.Intface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IExcelReader excel = new ExcelReader();
+            Revit.Addin.RevitTooltip.Dto.SheetInfo result = excel.loadExcelData("E:\\revit数据文档\\基础构件-JC-I.xls");
+            Console.WriteLine("下面打印ExcelTableData数据");
+            if (result.ExcelTableData != null) {
+                Console.WriteLine("signal:" + result.ExcelTableData.Signal);
+                Console.WriteLine("Desc:" + result.ExcelTableData.TableDesc);
+            }
+            Console.WriteLine("打印是否为属性数据的标志位："+result.Tag);
+
+            if (result.EntityNames != null) {
+            Console.WriteLine("打印实例名列表:列表总数位"+result.EntityNames.Count());
+                foreach(string i in result.EntityNames){
+                    Console.Write(i + "\t");
+                }
+            }
+            Console.WriteLine();
+            if (result.KeyNames != null) {
+            Console.WriteLine("打印属性名列表：列表长度为："+result.KeyNames.Count());
+                foreach (string i in result.KeyNames) {
+                    Console.Write(i + "\t");
+                }
+            }
+            Console.WriteLine();
+            if(result.InfoRows != null){
+            Console.WriteLine("打印属性数据::实体数"+result.InfoRows.Count());
+                foreach (InfoEntityData info in result.InfoRows) {
+                    Console.WriteLine(info.EntityName+"--->对应属性数据总共:"+info.Data.Count());
+                    foreach (string key in info.Data.Keys) {
+                        Console.WriteLine(key + ">>" + info.Data[key]);
+                    }
+
+                }
+            }
+            Console.WriteLine();
+            if (result.DrawRows != null) {
+            Console.WriteLine("打印测量数据::测量数据的总数"+result.DrawRows.Count());
+                foreach (DrawEntityData d in result.DrawRows) {
+                    Console.WriteLine("测量实体："+d.EntityName+"对应数据条数："+d.Data.Count());
+                    foreach (DrawData k in d.Data) {
+                        Console.WriteLine("时间：："+k.Date+"\t最大值::"+k.MaxValue+"\t最小值::"+k.MinValue+"\t中位数"+k.MidValue);
+                        Console.WriteLine("详细为：" + k.Detail);
+                    }
+                      }                  
+                        }
+            Console.ReadKey();
+        }
+    }
+}
